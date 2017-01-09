562890dce403b4d103fc21bcd250d76c42c8b19c
Grokwik <frederic@bodin.site>
2017-01-09 13:42:20 +0100

    More tests + working solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,35 @@
+import java.util.*;
+
 public class Solution {
 	
 	static boolean check(String str) {
-		// -----------------------
-		//
-		//  INSERT YOUR CODE HERE
-		//
-		// -----------------------
-		return true;
+        if (str == null)
+            return true;
+
+        int idx = 0;
+        List<Character> Possible = new ArrayList<Character>();
+
+        while (idx < str.length())
+        {
+            switch (str.charAt(idx))
+            {
+                case '(':
+                    Possible.add(')');
+                    break;
+                case '[':
+                    Possible.add(']');
+                    break;
+                case ']':
+                case ')':
+                    if ((Possible.size() == 0)
+                      ||((char)Possible.get(Possible.size() - 1) != str.charAt(idx)))
+                        return false;
+                    Possible.remove(Possible.size() - 1);
+                    break;
+            }
+            idx++;
+        }
+        return (Possible.size() == 0);
 	}
 
 	public static void main(String[] args) {
--- src/TestCases.java
+++ src/TestCases.java
@@ -3,49 +3,59 @@ import org.junit.Test;
 
 public class TestCases {
 	@Test
-	public void Test1() {
+	public void Test01() {
 		assertEquals("Well formatted string", true, Solution.check("[()]"));
 	}
 
 	@Test
-    public void Test2() {
+    public void Test02() {
 		assertEquals("Well formatted string (2)", true, Solution.check("(()[])"));
     }
 
 	@Test
-	public void Test3() {
+	public void Test03() {
 		assertEquals("Bracket error", false, Solution.check("([)]"));
     }
 
 	@Test
-	public void Test4() {
-		assertEquals("Parenthesis error", false, Solution.check("(("));
+	public void Test04() {
+		assertEquals("Parenthesis error", false, Solution.check("g((ggt"));
     }
 
 	@Test
-	public void Test5() {
-		assertEquals("Parenthesis error (2)", false, Solution.check("[(()])"));
+	public void Test05() {
+		assertEquals("Parenthesis error (2)", false, Solution.check("[((coucou)])"));
 	}
 
 	@Test
-	public void Test6() {
-		assertEquals("Well formatted string (3)", true, Solution.check("([(([[(([]))]]))])"));
+	public void Test06() {
+		assertEquals("Well formatted string (3)", true, Solution.check("([(([[((ddd[thg]qsqs))]]))])"));
     }
 
 	@Test
-	public void Test7() {
-		assertEquals("Well formatted string (4)", true, Solution.check("[](()()[[]])()[]([])")); // true
+	public void Test07() {
+		assertEquals("Well formatted string (4)", true, Solution.check("[](()dq()[[]]qsqsq)()[]([])")); // true
     }
 
 	@Test
-	public void Test8() {
+	public void Test08() {
 		assertEquals("Missing parenthesis", false, Solution.check("([((([(([]))])))))])"));
     }
 
 	@Test
-	public void Test9() {
+	public void Test09() {
 		assertEquals("Nested", false, Solution.check("[](()()[[]])[][[([])"));
     }
 
+	@Test
+	public void Test10() {
+		assertEquals("Reliability ?", true, Solution.check(null));
+	}
 
+	@Test
+	public void Test11() {
+		assertEquals("Too much closings", false, Solution.check("([fdg]))))"));
+	}
 }
+
+
